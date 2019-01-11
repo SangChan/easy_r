4f712e9f05906e8795aa40f36e1c5dcc72e8528f
SangChan Lee <sangchan82@gmail.com>
2019-01-11 15:45:42 +0900

    add new chapter

---
+++ script3.R
@@ -0,0 +1,51 @@
+a <- 1
+a
+b <- 2
+b
+c <- 3
+c
+d <- 3.5
+d
+a+b
+a+b+c
+4/b
+5*b
+
+var1 <- c(1,2,5,7,8)
+var1
+var2 <- c(1:5)
+var2
+var3 <- seq(1,5)
+var3
+var4 <- seq(1,10, by=2)
+var4
+var5 <- seq(1,10, by=3)
+var5
+var1+2
+var1+var2
+#var4+var5
+
+str1 <- "a"
+str1
+str2 <- "text"
+str2
+str3 <- "Hello World!"
+str3
+str4 <- c("a","b","c")
+str4
+str5 <- c("Hello!","World","is","good!")
+str5
+#str1+2
+paste(str5,collapse = ",")
+paste(str5,collapse = " ")
+
+x <- c(1,2,3)
+mean(x)
+max(x)
+min(x)
+
+x_mean <- mean(x)
+x_mean
+
+str5_paste <- paste(str5, collapse = " ")
+str5_paste
