6b16ce4e3cb6d80522d8224b5dd1f777e28a7254
Sangchan Lee <sangchan.lee@ef.com>
2019-01-19 04:19:31 +0900

    add script for chapter9

---
+++ script9.R
@@ -0,0 +1,25 @@
+#9.1
+install.packages("foreign")
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+raw_welfare <- read.spss(file = "Koweps_hpc10_2015_beta1.sav",
+                         to.data.frame = TRUE)
+welfare <- raw_welfare
+head(welfare)
+tail(welfare)
+#View(welfare)
+dim(welfare)
+str(welfare)
+summary(welfare)
+welfare <- rename(welfare,
+                  sex = h10_g3,
+                  birth = h10_g4,
+                  marriage = h10_g10,
+                  religion = h10_g11,
+                  income = p1002_8aq1,
+                  code_job = h10_eco9,
+                  code_region = h10_reg7)
+
+#9.2
