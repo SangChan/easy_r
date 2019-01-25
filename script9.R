e6e5a09a637d19a4f08ff61adfb4cdacf416dd14
Sangchan Lee <sangchan.lee@ef.com>
2019-01-25 11:54:19 +0900

    add another cases

--- script9.R
+++ script9.R
@@ -119,7 +119,18 @@ bottom10
 ggplot(data = bottom10, aes(x=reorder(job,-mean_income),y=mean_income)) + geom_col() + coord_flip() + ylim(0,150)
 #9.7
 job_male <- welfare %>%
-  filter(!is.na(job)) %>%
+  filter(!is.na(job) & sex == "male") %>%
   group_by(job) %>%
   summarise(n = n()) %>%
-  arrange(desc(n))
+  arrange(desc(n)) %>%
+  head(10)
+job_male
+job_female <- welfare %>%
+  filter(!is.na(job) & sex == "female") %>%
+  group_by(job) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  head(10)
+job_female
+ggplot(data = job_male, aes(x=reorder(job, n), y = n)) + geom_col() + coord_flip()
+ggplot(data = job_female, aes(x=reorder(job, n), y = n)) + geom_col() + coord_flip()
