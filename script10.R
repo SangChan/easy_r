edf888f8c73153f7e10d19b5cb88bac377daebd1
Sangchan Lee <sangchan.lee@ef.com>
2019-01-28 16:17:08 +0900

    so hard to init rJava

--- script10.R
+++ script10.R
@@ -1,5 +1,9 @@
 #10-1
+remove.packages("rJava")
 install.packages("rJava")
 install.packages("memoise")
 install.packages("KoNLP")
+Sys.setenv(JAVA_HOME ="/Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home/jre")
+dyn.load("/Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home/jre/lib/server/libjvm.dylib")
+library(rJava)
 library(KoNLP)
